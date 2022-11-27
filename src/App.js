efd8a1dc33c5c9020035bee50708234eed60fe2b
jjo-mi <whdal2636@naver.com>
2022-11-27 17:43:19 +0900

    feat: 2. 컴퓨터 값 생성(랜덤 3자리)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ const {GUIDE_MESSAGE, RESULT, ERROR_MESSAGE } = require("./Constant");
 
 class App {
   play() {
+    /** 1. 게임 시작 안내문구 */
     Console.print(GUIDE_MESSAGE.START);
+
+    /**2. 랜덤 숫자 3개 생성 */
+    const computerArr = [];
+    while (computerArr.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerArr.includes(number)) {
+        computerArr.push(number);
+      }
+    }
+    
+    /**3. */
+  
+
 
 
 
