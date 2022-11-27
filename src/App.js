4dcdc696564fe87515ed9d32482f7d986b8d763a
jjo-mi <whdal2636@naver.com>
2022-11-27 17:28:02 +0900

    feat 1. 게임시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const {GUIDE_MESSAGE, RESULT, ERROR_MESSAGE } = require("./Constant");
+
 class App {
-  play() {}
+  play() {
+    Console.print(GUIDE_MESSAGE.START);
+
+
+
+
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
