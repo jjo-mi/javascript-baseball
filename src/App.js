97356edf48f00d27fe25091065225734a3788480
jjo-mi <whdal2636@naver.com>
2022-11-28 21:28:18 +0900

    feat: 4. 컴퓨터 값과 사용자 입력값 비교

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
-const {GUIDE_MESSAGE, RESULT, ERROR_MESSAGE } = require("./Constant");
+const { GUIDE_MESSAGE, RESULT, ERROR_MESSAGE } = require("./Constant");
+
+let ballCount = 0;
+let strikeCount = 0;
 
 class App {
+  constructor() {
+    this.ballCount = 0;
+    this.strikeCount = 0;
+  }
+
   play() {
     /** 1. 게임 시작 안내문구 */
     Console.print(GUIDE_MESSAGE.START);
 
-    /**2. 랜덤 숫자 3개 생성 */
-    const computerArr = [];
+    /** 2. 랜덤 숫자 3개 생성 */
+    let computerArr = [];
     while (computerArr.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
       if (!computerArr.includes(number)) {
         computerArr.push(number);
       }
     }
+    console.log('###com: ', computerArr);
 
-    /**3. 사용자에게 숫자 입력 받기*/
-    Console.readLine(GUIDE_MESSAGE.INPUTNUMBER, (input) => {
-      // 비교한 결과값 출력);
+    /** 3. 사용자에게 숫자 입력 받기*/
+    Console.readLine('숫자를 입력해주세요 : ', (input) => {
+      let userArr = input.split("").map(Number);
+      console.log(userArr);
+      this.compareInput(computerArr, userArr);
+      this.showResult(ballCount, strikeCount);
     });
-
-  
-
-
-
-
-
   }
+
+  /** 4. 컴퓨터 값과 사용자 입력값 비교 */
+  compareInput(computerArr, userArr) {
+    for(let i = 0; i < computerArr.length; i++) {
+      if(computerArr[i] === userArr[i]) {
+        this.strikeCount++;
+      } else if(computerArr.includes(userArr[i])) {
+        this.ballCount++;
+      }
+    }
+    };
+
 }
+
+
 
 const app = new App();
 app.play();
