debbf9cecd2d9fbe638daf6c5a1b14b7a461688d
jjo-mi <whdal2636@naver.com>
2022-12-15 22:48:05 +0900

    feat: 1. 게임시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const { GUIDE_MESSAGE, RESULT_KEY, ERROR_MEASSAGE } = require("./Constant");
+
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.")
+    
+  }
 }
+
+const app = new App;
+app.play();
 
 module.exports = App;
