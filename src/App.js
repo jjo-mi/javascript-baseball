6bf328a752f8f7575126265c2e6fe3fed49c87b5
jjo-mi <whdal2636@naver.com>
2022-12-15 22:58:32 +0900

    feat: 3. 사용자에게 입력값 받기

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.")
     this.creatComputerNumbers();
+    this.inputNumber();
 
   }
   /** 2. 컴퓨터 숫자 생성 */
   creatComputerNumbers() {
     const computerArr = Random.pickUniqueNumbersInRange(1, 9, 3);
     console.log("###", computerArr);
+  }
+  /** 3. 사용자 입력값 받기 */
+  inputNumber() {
+    Console.readLine(GUIDE_MESSAGE.INPUT_NUMBER, (numbers) => {
+      console.log('###', typeof numbers);
+      //유효성 검토
+    });
+
   }
 }
 
