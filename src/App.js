7725a4c1bcfb84004e7389ba530896255186946f
jjo-mi <whdal2636@naver.com>
2022-12-15 22:52:48 +0900

    feat: 2. 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ const { GUIDE_MESSAGE, RESULT_KEY, ERROR_MEASSAGE } = require("./Constant");
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.")
-    
+    this.creatComputerNumbers();
+
+  }
+  /** 2. 컴퓨터 숫자 생성 */
+  creatComputerNumbers() {
+    const computerArr = Random.pickUniqueNumbersInRange(1, 9, 3);
+    console.log("###", computerArr);
   }
 }
+
+
+  
+
+
+
+
 
 const app = new App;
 app.play();
